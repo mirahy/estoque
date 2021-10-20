a43b94ad44cf52935aff3d3aaa2cdae99a93ddba
Nicholas Eduardo Lopes dos Santos <nicholaseduardo@yahoo.com.br>
2021-10-20 16:56:42 -0400

    Adição das classes

---
+++ src/main/java/br/edu/ifms/estoque/model/GrupoProduto.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifms.estoque.model;
+
+/**
+ *
+ * @author santos
+ */
+public class GrupoProduto {
+    private Long id;
+    private String nome;
+    private GrupoProduto subgrupo;
+
+    public GrupoProduto() {
+    }
+
+    public GrupoProduto(Long id, String nome, GrupoProduto subgrupo) {
+        this.id = id;
+        this.nome = nome;
+        this.subgrupo = subgrupo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public GrupoProduto getSubgrupo() {
+        return subgrupo;
+    }
+
+    public void setSubgrupo(GrupoProduto subgrupo) {
+        this.subgrupo = subgrupo;
+    }
+}
---
+++ src/main/java/br/edu/ifms/estoque/model/UnidadeMedida.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifms.estoque.model;
+
+/**
+ *
+ * @author santos
+ */
+public class UnidadeMedida {
+    private Long id;
+    private String nome;
+
+    public UnidadeMedida() {
+    }
+
+    public UnidadeMedida(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
